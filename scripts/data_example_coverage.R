7ee045021d7f87a73cb3f9b08b7d2839517d80ea
Emma Thomas <emmathomas@g.harvard.edu>
2019-08-14 18:41:31 -0400

    First simulations completed

--- scripts/data_example_coverage.R
+++ scripts/data_example_coverage.R
@@ -7,14 +7,10 @@
 direc <- "/nfs/home/E/ethomas/shared_space/ci3_nsaph/Emma/R_code/moretrees"
 setwd(direc)
 
-if(!dir.exists("./data_example_results")) dir.create("./data_example_results")
-
 ### Load functions ###
 source("scripts/VI_functions.R")
 source("scripts/processing_functions.R")
 require(igraph)
-require(Matrix)
-require(doParallel)
 
 ### Load ICD9 tree ###
 load("./simulation_inputs/inputs.Rdata")
@@ -38,7 +34,7 @@ load(file="/nfs/home/E/ethomas/shared_space/ci3_nsaph/Emma/Data/moretrees_data/m
 # Load original results
 load(file="./data_example_results/data_example_full.Rdata")
 # Use the collapsed estimates as true betas in simulation
-beta_true <- beta_est
+beta_true <- final_ss$moretrees_est
 # Use other estimates as initial values for VI algorithm in simulation
 mu_gamma_init <- final_ss$VI_params$mu_gamma
 sigma2_gamma_init <- final_ss$VI_params$sigma2_gamma
@@ -46,18 +42,6 @@ u_s_init <- final_ss$VI_params$u_s
 tau_init <- final_ss$hyperparams[2]
 rho_init <- final_ss$hyperparams[1]
 
-###################### Simulate case/control status ####################
-
-# Simulate exposures and case/control status\
-Z.sim <- list()
-for(v in 1:pL){
-  # Compute probability unit 1 is a case
-  prob_unit1 <- 1/(1+exp(-beta_true[v]*Z[[v]]))
-  probs <- as.data.frame(rbind(prob_unit1,1-prob_unit1))
-  # Randomly assign case or control status
-  cc <- sapply(probs,sample,x=c(1,-1),size=1,replace=FALSE)
-  Z.sim[[v]] <- Z[[v]]*cc
-}
 
 ######################## Variational inference ########################
 
@@ -69,7 +53,7 @@ VI_file <- paste0("simulation_results/coverage/VI_coverage_block",block,".csv")
 hyper_file <- paste0("simulation_results/coverage/hyper_coverage_block",block,".csv")
 
 if(file.exists(beta_file)){
-  i <- nrow(read.(beta_file))
+  i <- nrow(read.csv(beta_file))
 } else {
   file.create(beta_file)
   i <- 0
@@ -79,15 +63,27 @@ if(!file.exists(hyper_file)) file.create(hyper_file)
 
 
 # saving output to track number of time steps
-pr <- paste0("./simulation_results/coveage/print_coverage_block",block,".txt")
+pr <- paste0("./simulation_results/coverage/print_coverage_block",block,".txt")
 sink(file=pr)
 
-while(i < nsims){
+# create list for simulated data
+Z.sim <- Z
+
+while(i <= nsims){
+  
+  # Simulate exposures and case/control status\
+  for(v in 1:pL){
+    # Compute probability unit 1 is a case
+    prob_unit1 <- 1/(1+exp(-beta_true[v]*Z[[v]]))
+    # Randomly assign case or control status
+    cc <- runif(Y[v]) < prob_unit1
+    Z.sim[[v]] <- Z[[v]]*(2*cc-1)
+  }
   
   # run VI algorithm
-  out_vi <- VI_binary_ss(Z=Z.sim,Y=Y,n=nsamp,p=p,pL=pL,ancestors=ancestors,
+  out_vi <- VI_binary_ss(Z=Z.sim,Y=Y,n=sum(Y),p=p,pL=pL,ancestors=ancestors,
                          leaf.descendants=leaf.descendants,cutoff=0.5,
-                         mu_gamma_init=nodes_init,u_s_init=u_s_init,sigma2_gamma_init=sigma2_gamma_init,
+                         mu_gamma_init=mu_gamma_init,u_s_init=u_s_init,sigma2_gamma_init=sigma2_gamma_init,
                          tau_init=tau_init,rho_init=rho_init,
                          tol=tol,m.max=m.max,m.print=m.print,more=FALSE,update_hyper=T,update_hyper_freq=10)
   
@@ -96,14 +92,11 @@ while(i < nsims){
   cat("\n\nCompleted simulation",i,"\n\n")
   
   # write output to csv file
-  write.table(rbind(c(i,out_ss$moretrees_est)), file = beta_file, row.names =FALSE, col.names = FALSE,sep = ",", append = TRUE)
-  close(outfile_hyperparams)
+  write.table(rbind(c(i,out_vi$moretrees_est)), file = beta_file, row.names =FALSE, col.names = FALSE,sep = ",", append = TRUE)
   
   write.table(cbind(rep(i,p),out_vi$VI_params), file = VI_file, row.names =FALSE, col.names = FALSE,sep = ",", append = TRUE)
-  close(outfile_VI)
   
-  write.table(rbind(c(i,out_ss$hyperparams)), file = hyper_file, row.names =FALSE, col.names = FALSE,sep = ",", append = TRUE)
-  close(outfile_hyperparams)
+  write.table(rbind(c(i,out_vi$hyperparams)), file = hyper_file, row.names =FALSE, col.names = FALSE,sep = ",", append = TRUE)
   
 }
 
